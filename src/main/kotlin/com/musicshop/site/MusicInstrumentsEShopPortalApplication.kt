9c362c6173d845110b146eac57210eb99b164b1e
Motozov Artyom <motozov.a.v@gmail.com>
2022-07-23 23:27:40 +0300

    [-] Init commit

---
+++ src/main/kotlin/com/musicshop/site/MusicInstrumentsEShopPortalApplication.kt
@@ -0,0 +1,11 @@
+package com.musicshop.site
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusicInstrumentsEShopPortalApplication
+
+fun main(args: Array<String>) {
+	runApplication<MusicInstrumentsEShopPortalApplication>(*args)
+}
